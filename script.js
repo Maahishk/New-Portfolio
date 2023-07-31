9bcb4b2ae27c0d6fc6eeafe770f79e52121ef6a3
Maahishk <mahimalaxmishakya@gmail.com>
2023-07-31 23:08:01 +0545

    mobile reponsive

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  if ($(window).width() < 800) {
+    console.log($(window).width());
+    $(".menu-bar").removeClass("fd-none");
+    $("nav").addClass("fd-none");
+  }
+});
+
+let section = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
