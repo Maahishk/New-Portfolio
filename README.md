2674710170b629284c1b5f13ff7406f2e7d1802a
Maahishk <mahimalaxmishakya@gmail.com>
2023-08-01 15:52:19 +0545

    skills and tools

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # New-Portfolio
+
+Simple html css portfolio
+
+For Scroll animation
+
+ <script src="https://unpkg.com/scrollreveal"></script>
+
+For high quality icons
+
+  <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
--- script.js
+++ script.js
@@ -33,23 +33,23 @@ $(document).ready(function () {
   }
 });
 
-let section = document.querySelectorAll("section");
-let navlinks = document.querySelectorAll("header nav a");
+// let section = document.querySelectorAll("section");
+// let navlinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  section.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+// window.onscroll = () => {
+//   section.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-};
+//     if (top >= offset && top < offset + height) {
+//       navlinks.forEach((links) => {
+//         links.removeClass("active");
+//         document
+//           .querySelector("header nav a[href*=" + id + "]")
+//           .addClass("active");
+//       });
+//     }
+//   });
+// };
